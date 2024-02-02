2d2f5da51675c243d48007485b51b1ff234e8603
Aiwin Charalil Manuel <aw380590@dal.ca>
2024-02-01 21:15:07 -0400

    edited

--- A1/app1/app.py
+++ A1/app1/app.py
@@ -5,28 +5,26 @@ import requests
 app = Flask(__name__)
 
 @app.route('/calculate', methods=['POST'])
-def calculate():
+#verfiying file existance
+def verification():
     data = request.get_json()
+    name = data['file']
+    path = os.path.join('/data', name)
 
     if not data or 'file' not in data:
-        return jsonify({"file": None, "error": "Invalid JSON input."})
+        return jsonify({"file": None,"error": "Invalid JSON input."})
 
-    file_name = data['file']
-    file_path = os.path.join('/data', file_name)
-
-    if not os.path.exists(file_path):
-        return jsonify({"file": file_name, "error": "File not found."})
+    if not os.path.exists(path):
+        return jsonify({"file": name,"error": "File not found."})
 
     product = data.get('product')
-    response = forward_to_container_2(file_path, product)
+    response = data_fetching(path, product)
 
     return response
-
-def forward_to_container_2(file_path, product):
-    container2_url = "http://app2:6002/calculate"
+#forwarding to 2nd container
+def data_fetching(file_path, product):
+    url = "http://app2:6002/calculate"
     data = {"file": os.path.basename(file_path), "product": product}
-    response = requests.post(container2_url, json=data)
+    response = requests.post(url, json=data)
     return response.json()
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=6000)
--- A1/app2/app.py
+++ A1/app2/app.py
@@ -3,34 +3,30 @@ import csv
 import os
 
 app = Flask(__name__)
-
 @app.route('/calculate', methods=['POST'])
-def calculate():
+def get_data():
     data = request.get_json()
-    file_name = data.get('file')
+    # fetching file name
+    name = data.get('file')
     product = data.get('product')
+    path = os.path.join('/data', name)
 
-    if not file_name:
+    if not name:
         return jsonify({"file": None, "error": "Invalid JSON input."})
+    
+    if not os.path.exists(path):
+        return jsonify({"file": name, "error": "File not found."})
 
-    file_path = os.path.join('/data', file_name)
-
-    if not os.path.exists(file_path):
-        return jsonify({"file": file_name, "error": "File not found."})
-
-    return calculate_sum(file_path, product)
-
-def calculate_sum(file_path, product):
+    return sum(path, product)
+#Calculating sum
+def sum(file_path, product):
     try:
         with open(file_path, 'r') as dat_file:
-            csv_reader = csv.DictReader(dat_file)
+            file_data = csv.DictReader(dat_file)
             sum = 0
-            for row in csv_reader:
+            for row in file_data:
                 if row['product'] == product:
                     sum += int(row['amount'])
             return jsonify({"file": os.path.basename(file_path), "sum": sum})
     except Exception as e:
         return jsonify({"file": os.path.basename(file_path), "error": "Input file not in CSV format."})
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=6002)
