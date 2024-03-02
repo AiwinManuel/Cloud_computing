92604c298ed2680c66c050091acae2adacbdd752
Aiwin Charalil Manuel <aw380590@dal.ca>
2024-03-02 18:17:15 -0400

    kube

---
+++ K8s/k1/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import os
+import requests
+
+app = Flask(__name__)
+DATA_DIR = "/aiwin_PV_dir"  
+
+@app.route('/store-file', methods=['POST'])
+def store_file():
+    data = request.get_json()
+    if not data or 'file' not in data or 'data' not in data:
+        return jsonify({"file": None, "error": "Invalid JSON input."})
+
+    file_path = os.path.join(DATA_DIR, data['file'])
+    try:
+        with open(file_path, 'w') as file:
+            file.write(data['data'])
+        return jsonify({"file": data['file'], "message": "Success."})
+    except Exception as e:
+        return jsonify({"file": data['file'], "error": "Error while storing the file to the storage."})
+
+@app.route('/calculate', methods=['POST'])
+def verification():
+    data = request.get_json()
+    if not data or 'file' not in data:
+        return jsonify({"file": None, "error": "Invalid JSON input."})
+
+    file_path = os.path.join(DATA_DIR, data['file'])
+    if not os.path.exists(file_path):
+        return jsonify({"file": data['file'], "error": "File not found."})
+
+    return data_fetching(file_path, data.get('product'))
+
+def data_fetching(file_path, product):
+    url = "http://app2-service:6002/calculate"  # Adjust the URL as per your setup
+    data = {"file": os.path.basename(file_path), "product": product}
+    response = requests.post(url, json=data)
+    return response.json()
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5001)
---
+++ K8s/k2/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import csv
+import os
+
+app = Flask(__name__)
+DATA_DIR = "/aiwin_PV_dir"  
+
+@app.route('/calculate', methods=['POST'])
+def get_data():
+    data = request.get_json()
+    if not data or 'file' not in data:
+        return jsonify({"file": None, "error": "Invalid JSON input."})
+
+    file_path = os.path.join(DATA_DIR, data['file'])
+    if not os.path.exists(file_path):
+        return jsonify({"file": data['file'], "error": "File not found."})
+
+    return calculate_sum(file_path, data.get('product'))
+
+def calculate_sum(file_path, product):
+    try:
+        with open(file_path, 'r') as dat_file:
+            file_data = csv.DictReader(dat_file)
+            total_sum = 0
+            for row in file_data:
+                if row['product'] == product:
+                    total_sum += int(row['amount'])
+            return jsonify({"file": os.path.basename(file_path), "sum": total_sum})
+    except Exception as e:
+        return jsonify({"file": os.path.basename(file_path), "error": "Input file not in CSV format."})
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=6002)
